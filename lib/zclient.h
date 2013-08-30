d0fd2089e0ee90e3f960fb6f81510cda87ffe50c
indula <indula.mrt@gmail.com>
2013-08-30 23:17:13 +0530

    added read_zebra_daemon

--- lib/zclient.h
+++ lib/zclient.h
@@ -25,6 +25,9 @@
 /* For struct zapi_ipv{4,6}. */
 #include "prefix.h"
 
+/*For nexthop in zapi-route*/
+#include "zebra/rib.h"
+
 /* For struct interface and struct connected. */
 #include "if.h"
 
@@ -124,6 +127,27 @@ struct zapi_ipv4
   u_int32_t metric;
 };
 
+struct zapi_route
+{
+  u_char type;
+
+  u_char flags;
+
+  u_char message;
+
+  safi_t safi;
+
+  u_char nexthop_num;
+
+  u_char ifindex_num;
+
+  struct nexthop  *nexthop;
+
+  u_char distance;
+
+  u_int32_t metric;
+};
+
 /* Prototypes of zebra client service functions. */
 extern struct zclient *zclient_new (void);
 extern void zclient_init (struct zclient *, int);
@@ -158,7 +182,9 @@ extern void zebra_interface_if_set_value (struct stream *, struct interface *);
 extern void zebra_router_id_update_read (struct stream *s, struct prefix *rid);
 extern int zapi_ipv4_route (u_char, struct zclient *, struct prefix_ipv4 *, 
                             struct zapi_ipv4 *);
-
+extern void zebra_init_route(struct zapi_route *, u_char , u_char,safi_t , int , long long int );
+extern void read_zebra_daemon(int ,struct zclient *, struct prefix *,struct stream *, struct zapi_route *);
+extern void add_nexthop_route(struct nexthop *,struct nexthop *);
 #ifdef HAVE_IPV6
 /* IPv6 prefix add and delete function prototype. */
 
